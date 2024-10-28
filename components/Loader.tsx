4728ce7dc3792321204b6f883e6f6a0bc82058d0
julioguirola <julioguirola03@gmail.com>
2024-10-27 21:45:51 -0400

    v1.1

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import "./Loader.css";
 export default function ({ visible }: { visible: boolean }) {
   return (
     <div
-      className={"relative m-auto " + (visible ? "visible" : "hidden")}
+      className={"loader relative m-auto " + (visible ? "visible" : "hidden")}
     ></div>
   );
 }
